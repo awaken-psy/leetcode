e9f8086565a6e993e60a85854edea444b4d8698a
awaken <1036840137@qq.com>
2026-03-31 11:07:12 +0800

    更新Claude.md

---
+++ hot100/二叉树/199.二叉树的右视图.cpp
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode.cn id=199 lang=cpp
+ *
+ * [199] 二叉树的右视图
+ *
+ * https://leetcode.cn/problems/binary-tree-right-side-view/description/
+ *
+ * algorithms
+ * Medium (73.21%)
+ * Likes:    1318
+ * Dislikes: 0
+ * Total Accepted:    809.2K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[1,2,3,null,5,null,4]'
+ *
+ * 给定一个二叉树的 根节点 root，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [1,2,3,null,5,null,4]
+ * 
+ * 输出：[1,3,4]
+ * 
+ * 解释：
+ * 
+ * 
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = [1,2,3,4,null,null,null,5]
+ * 
+ * 输出：[1,3,4,5]
+ * 
+ * 解释：
+ * 
+ * 
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = [1,null,3]
+ * 
+ * 输出：[1,3]
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 
+ * 输入：root = []
+ * 
+ * 输出：[]
+ * 
+ * 
+ * 
+ * 
+ * 提示:
+ * 
+ * 
+ * 二叉树的节点个数的范围是 [0,100]
+ * -100 <= Node.val <= 100 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        
+    }
+};
+// @lc code=end
+
