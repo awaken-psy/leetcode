9f9dcc73f332e959de94218e0064e16aea0b19c2
awaken <1036840137@qq.com>
2026-09-20 11:37:58 +0800

    2026/9/20

--- hot100/多维动态规划/64.最小路径和.cpp
+++ hot100/多维动态规划/64.最小路径和.cpp
@@ -57,6 +57,7 @@ class Solution {
         int n = grid[0].size();
 
         vector<vector<int>> dp(m, vector<int>(n, 1));
+        
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (i == 0 && j == 0) {
